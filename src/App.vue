47204cece62096080972e7112e68804fdd3d0be1
akh-ild <ildar.akhmadeyev@gmail.com>
2025-01-14 16:57:21 +0500

    init 2

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="title">Expence tracker</div>
+  </header>
+</template>
+
+<style scoped>
+.title {
+  font-size: 32px;
+}
+</style>
